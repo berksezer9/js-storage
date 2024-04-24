1117373bebea3d9438d6b5e32bb6387d15e7849d
berksezer9 <berksezer9@gmail.com>
2024-04-24 17:55:17 +0300

    fix: type GeneralPrefixes = string

--- src/storage/session-storage/TypesAndInterfaces.ts
+++ src/storage/session-storage/TypesAndInterfaces.ts
@@ -4,4 +4,4 @@ export interface StorageItems {[key: string]: StorageItem}
 export interface TransferKey {sender: string, recipient: string}
 export type TransferKeys = TransferKey[]
 //a string enum will be used in practice, instead
-export type GeneralPrefixes = {[key: string]: string};
+export type GeneralPrefixes = string;
